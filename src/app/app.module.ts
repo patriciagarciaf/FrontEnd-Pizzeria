124743dd74ce71c6f3a772ea002a102a638f12cd
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-20 11:30:03 +0200

    core created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { MenuSharedModule } from '../../projects/menu/src/app/app.module';
 import { IngredientSharedModule } from 'projects/ingredient/src/app/app.module';
 import { UserSharedModule } from 'projects/user/src/app/app.module';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
