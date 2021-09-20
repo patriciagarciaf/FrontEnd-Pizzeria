de2915e356559b6b2e463c1b9a6e4edc8e33c720
DamasoGB <dimnas12@gmail.com>
2021-09-20 09:29:24 +0200

    eliminado project menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuSharedModule } from '../../projects/menu/src/app/app.module';
 import { IngredientSharedModule } from 'projects/ingredient/src/app/app.module';
 import { UserSharedModule } from 'projects/user/src/app/app.module';
 
@@ -14,7 +13,6 @@ import { UserSharedModule } from 'projects/user/src/app/app.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MenuSharedModule.forRoot(),
     IngredientSharedModule.forRoot(),
     UserSharedModule.forRoot()
   ],
