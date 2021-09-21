3213ac1e268f505941fb6649c8b292a56068f564
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-21 11:30:00 +0200

    fixed

--- projects/core-library/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of core-library
- */
-
-export * from './lib/core-library.service';
-export * from './lib/core-library.component';
-export * from './lib/core-library.module';
-export * from './lib/menu/menu.component';
--- projects/pizza/src/app/app.module.ts
+++ projects/pizza/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { CoreLibraryModule } from 'projects/core-library/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
