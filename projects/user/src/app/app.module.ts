eca9d4e6eed48a85a36258070852b22a7f13ec98
DamasoGB <dimnas12@gmail.com>
2021-09-21 12:49:42 +0200

    sin error

--- projects/user/src/app/app.module.ts
+++ projects/user/src/app/app.module.ts
@@ -7,10 +7,7 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
-=======
->>>>>>> main
 
 const providers: any[] = [];
 
@@ -24,10 +21,7 @@ const providers: any[] = [];
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
     ReactiveFormsModule
-=======
->>>>>>> main
   ],
   providers: providers,
   bootstrap: [AppComponent]
