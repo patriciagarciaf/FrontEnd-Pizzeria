dc95d1883c217b5184b24db78bfd6213a896a8cd
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-24 09:09:08 +0200

    half done

--- projects/core-library/src/lib/components/autorize/authorize.ts
+++ projects/core-library/src/lib/components/autorize/authorize.ts
@@ -1,11 +1,31 @@
-import {Decorat}
+import { DecoratorService } from "../../interceptor/decoratorservice";
+
 
 function decorateClass(target:Function){
     const methods=Object.getOwnPropertyNames(target.prototype).filter(prop=> prop !="constructor");
     for (let method of methods){
         const fn=target.prototype[method];
         target.prototype[method]=function(...args:[]){
-            const autorizationObserver=
+            const autorizationObserver=DecoratorService.getAuthorizationObserver();
+            autorizationObserver.addToken = true;
+            const observer = fn.apply(this, args);
+            return observer;
         }
+    }
+}
+
+function decorateMethod(target: Function, propertyKey: string, descriptor: PropertyDescriptor) {
+    const fn = descriptor.value;
+    descriptor.value = function (...args: []) {
+        const autozitationObserver = DecoratorService.getAuthorizationObserver();
+        autozitationObserver.addToken = true;               
+        const observer = fn.apply(this, args)            
+        return observer
+    }
+}
+
+export function Authorize(): any {
+    return (target: Function, propertyKey: string, descriptor: PropertyDescriptor) => {
+        propertyKey ? decorateMethod(target, propertyKey, descriptor) : decorateClass(target)
     }
 }
