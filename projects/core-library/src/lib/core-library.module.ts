249d6067b12e057eea205f03c704e785af9d9690
DamasoGB <dimnas12@gmail.com>
2021-09-27 14:57:39 +0200

    para de dar error

--- projects/core-library/src/lib/core-library.module.ts
+++ projects/core-library/src/lib/core-library.module.ts
@@ -4,10 +4,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-<<<<<<< HEAD
-=======
 import { InputComponent } from './components/input/input.component';
->>>>>>> main
 import { ButtonComponent } from './components/button/button.component';
 
 
@@ -19,11 +16,7 @@ import { ButtonComponent } from './components/button/button.component';
     SpinnerComponent,
     TopBarComponent,
     PageNotFoundComponent,
-<<<<<<< HEAD
-=======
     InputComponent,
->>>>>>> main
-    ButtonComponent
   ],
   imports: [
   ],
