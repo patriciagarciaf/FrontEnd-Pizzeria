701c21459692a2098dfe406f5a2ddd67862dbf34
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-29 09:03:38 +0200

    spinner with decorator

--- projects/core-library/src/lib/services/loading.service.ts
+++ projects/core-library/src/lib/services/loading.service.ts
@@ -3,25 +3,18 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import {distinctUntilChanged} from 'rxjs/operators';
 
 const indicatorSubject = new BehaviorSubject<boolean>(false);
-
 export const isLoading$ = indicatorSubject.asObservable().pipe(distinctUntilChanged());
-
 @Injectable({
   providedIn: 'root',
 })
-
 export class LoadingService {
-  
   show() {
     indicatorSubject.next(true);
   }
-  
   hide() {
     indicatorSubject.next(false);
   }
   get isLoading$(): Observable<boolean> {
     return isLoading$;
   }
-
-  
 }
