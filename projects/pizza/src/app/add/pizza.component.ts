69d833bb267721c2fffb1e268412026aed14e5b9
Irene Guillen <irene.guillen-zapata@...com>
2021-10-04 13:51:43 +0200

    add cosas

--- projects/pizza/src/app/add/pizza.component.ts
+++ projects/pizza/src/app/add/pizza.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class PizzaComponent implements OnInit {
+export class PizzaComponent implements OnInit, OnDestroy {
 
   constructor() { }
+  ngOnDestroy(): void {
+  }
 
   ngOnInit() {
   }
