0387910ee34cde35b914c06435864fffb902e999
DamasoGB <dimnas12@gmail.com>
2021-10-04 13:03:00 +0200

    arreglos tontos

--- projects/pizza/src/app/app.module.ts
+++ projects/pizza/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreLibraryModule } from 'projects/core-library/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { PizzaComponent } from './add/pizza.component';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 
 const providers: any[] = []
@@ -13,11 +15,13 @@ const providers: any[] = []
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     CoreLibraryModule
   ],
@@ -30,6 +34,7 @@ export class AppModule {
 
 @NgModule({})
 export class PizzaSharedModule {
+  constructor(private router: Router){}
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: AppModule,
