dcc4318e3b3adf020aea910caeec1ea9a1abe378
Irene Guillen <irene.guillen-zapata@...com>
2021-10-06 08:38:38 +0200

    navbar

---
+++ projects/core-library/src/lib/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+    ngOnInit() {
+        const ele = document.getElementById("check") as HTMLInputElement;
+        document.onkeydown = function(evt) {
+            evt = evt || window.event;
+            var isEscape = false;
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc");
+            }
+            if (isEscape) {
+                ele.checked = false;
+            }
+        };
+    }
+}
